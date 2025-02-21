66d9a30b438a858a74ec344c1a3b6cda5bfb28c5
deepdeb <debnath.pradeep619@gmail.com>
2025-02-21 19:56:47 +0530

    change in enviroment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,9 @@
 export const environment = {
   production: true,
+  // API_ROOT: 'http://103.184.242.66:8082/v1/',
+  // FilePath: 'http://103.184.242.66:8082/uploads/files/',
+  // ImagePath: 'http://103.184.242.66:8082/uploads/images/',
+
   API_ROOT: 'https://studionomy.com/v1/',
   FilePath: 'https://studionomy.com/uploads/files/',
   ImagePath: 'https://studionomy.com/uploads/images/',
